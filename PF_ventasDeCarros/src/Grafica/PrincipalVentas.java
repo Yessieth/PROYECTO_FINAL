f9419efcd1a53d723bf2d118702c40e959556c90
Yessieth <165859308+Yessieth@users.noreply.github.com>
2024-07-14 22:43:26 -0500

    updated

--- PF_ventasDeCarros/src/Grafica/PrincipalVentas.java
+++ PF_ventasDeCarros/src/Grafica/PrincipalVentas.java
@@ -29,6 +29,7 @@ import javax.swing.JEditorPane;
 import javax.swing.JMenu;
 import java.awt.Canvas;
 import java.awt.Label;
+import javax.swing.ImageIcon;
 
 public class PrincipalVentas extends JFrame {
 
@@ -67,7 +68,7 @@ public class PrincipalVentas extends JFrame {
 		
 		JPanel panel = new JPanel();
 		panel.setBackground(new Color(0, 0, 0));
-		panel.setBounds(0, 0, 857, 124);
+		panel.setBounds(0, 0, 857, 142);
 		contentPane.add(panel);
 		panel.setLayout(null);
 		
@@ -75,32 +76,46 @@ public class PrincipalVentas extends JFrame {
 		lblPty.setFont(new Font("Showcard Gothic", Font.PLAIN, 50));
 		panel.add(lblPty);
 		lblPty.setForeground(new Color(255, 0, 0));
-		lblPty.setBounds(111, 39, 150, 53);
+		lblPty.setBounds(115, 39, 150, 53);
 		
 		JLabel lblCarCenter = new JLabel("car center");
 		lblCarCenter.setFont(new Font("Magneto", Font.PLAIN, 35));
 		lblCarCenter.setForeground(new Color(255, 0, 0));
-		lblCarCenter.setBounds(209, 51, 228, 32);
+		lblCarCenter.setBounds(219, 51, 228, 32);
 		panel.add(lblCarCenter);
 		
-		JMenu mnNewMenu = new JMenu("menú");
-		mnNewMenu.setForeground(new Color(255, 255, 255));
-		mnNewMenu.setBounds(35, 88, 115, 26);
-		panel.add(mnNewMenu);
+		JLabel lblNewLabel_3 = new JLabel("");
+		lblNewLabel_3.setIcon(new ImageIcon("C:\\Users\\sirja\\Downloads\\Imagen7.png"));
+		lblNewLabel_3.setBounds(11, 26, 104, 71);
+		panel.add(lblNewLabel_3);
+		
+		JLabel lblNewLabel_4 = new JLabel("");
+		lblNewLabel_4.setIcon(new ImageIcon("C:\\Users\\sirja\\Downloads\\Imagen9.png"));
+		lblNewLabel_4.setBounds(684, 26, 139, 93);
+		panel.add(lblNewLabel_4);
+		
+		JLabel lblNewLabel_5 = new JLabel("Los mejores en ventas de autos...");
+		lblNewLabel_5.setBounds(125, 81, 248, 17);
+		panel.add(lblNewLabel_5);
+		lblNewLabel_5.setForeground(Color.WHITE);
+		lblNewLabel_5.setFont(new Font("STXingkai", Font.PLAIN, 20));
 		
 		JLabel lblNewLabel = new JLabel("Inicio");
-		mnNewMenu.add(lblNewLabel);
+		lblNewLabel.setBounds(60, 111, 35, 21);
+		panel.add(lblNewLabel);
 		lblNewLabel.setFont(new Font("Arial Unicode MS", Font.PLAIN, 15));
-		lblNewLabel.setForeground(new Color(0, 0, 0));
+		lblNewLabel.setForeground(Color.WHITE);
 		
 		JLabel lblNewLabel_1 = new JLabel("Inventario");
-		mnNewMenu.add(lblNewLabel_1);
+		lblNewLabel_1.setBounds(166, 111, 66, 21);
+		panel.add(lblNewLabel_1);
 		lblNewLabel_1.setFont(new Font("Arial Unicode MS", Font.PLAIN, 15));
-		lblNewLabel_1.setForeground(new Color(0, 0, 0));
+		lblNewLabel_1.setForeground(Color.WHITE);
 		
 		JLabel lblNewLabel_2 = new JLabel("Agregar carro");
-		mnNewMenu.add(lblNewLabel_2);
-		lblNewLabel_2.setForeground(new Color(0, 0, 0));
+		lblNewLabel_2.setBounds(304, 111, 94, 21);
+		panel.add(lblNewLabel_2);
+		lblNewLabel_2.setForeground(Color.WHITE);
 		lblNewLabel_2.setFont(new Font("Arial Unicode MS", Font.PLAIN, 15));
 	}
 }
